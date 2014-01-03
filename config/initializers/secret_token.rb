770d34e0a0102178d10cd9d6241d0630f173027e
Artem Sheremet <dot.doom@gmail.com>
2014-01-03 19:03:21 +0300

    create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JabberServiceAccounting::Application.config.secret_token = '49d9f85d404c14360d91a38bd3fb540dc73bd27da7baf76614197b002ea8ca795add1b12cb51f4112cff9853ea3ea77c43066df3a8f7bce4cdf416c7dca6a6f4'
