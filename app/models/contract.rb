a21925c223ccc5148ec667b7a72bc615b9a0626d
Artem Sheremet <dot.doom@gmail.com>
2014-01-05 00:01:17 +0300

    Move data helper methods into corresponding models

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,4 +3,12 @@ class Contract < ActiveRecord::Base
   belongs_to :seller, class_name: 'User'
   has_many :payments
   attr_accessible :name, :duration_months, :next_amount_estimate
+
+  def last_payment
+    @last_payment ||= payments.last
+  end
+
+  def next_payment_date
+    last_payment.try(:created_at).try(:+, duration_months.months)
+  end
 end
