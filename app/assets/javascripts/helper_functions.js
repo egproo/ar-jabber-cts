424ff767190d0ec5f1d98218f5fbb85f9f7c6536
Alexey Ershov <alex4rom@gmail.com>
2014-01-06 23:32:59 +0300

    Change helper functions

--- app/assets/javascripts/helper_functions.js
+++ app/assets/javascripts/helper_functions.js
@@ -1,13 +1,13 @@
-function userRenderer (data, type, row) {
+var userRenderer = function(data, type, row) {
   var displayName = data.role > 0 ? data.name : data.jid;
   return "<a href='/users/" + data.id + "'>" + displayName + "</a>";
-}
+};
 
-function dateRenderer (data, type, row) {
+var dateRenderer = function(data, type, row) {
   return new Date(Date.parse(data)).toLocaleDateString();
 };
 
-function paymentRenderer (data, type, row) {
+var paymentRenderer = function(data, type, row) {
   var nextDate = new Date(Date.parse(data));
   var tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
