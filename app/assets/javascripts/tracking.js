16085a3ea1264fa5de808e687513819de7f931d2
Alexey Ershov <alex4rom@gmail.com>
2014-01-06 18:49:30 +0300

    Add color highlighting to the 'Next Date' column

--- app/assets/javascripts/tracking.js
+++ app/assets/javascripts/tracking.js
@@ -1,28 +1,26 @@
-// $(document).ready(function() {
-//     $('#products').dataTable();
-// } );
-
 $(document).ready(function() {
 
-    var responsiveHelper;
-    var breakpointDefinition = {
-        tablet: 1024,
-        phone : 480
-    };
     var tableContainer = $('#contracts');
 
     var userRenderer = function(data, type, row) {
         var displayName = data.role > 0 ? data.name : data.jid;
         return "<a href='/users/" + data.id + "'>" + displayName + "</a>";
-    }
+    };
     var dateRenderer = function(data, type, row) {
         return new Date(Date.parse(data)).toLocaleDateString();
-    }
+    };
+    var paymentRenderer = function(data, type, row) {
+        var nextDate = new Date(Date.parse(data));
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        if (nextDate <= tomorrow) {
+            return '<span class="highlight_date">' + nextDate.toLocaleDateString() + '</span>';
+        } else {
+            return nextDate.toLocaleDateString();
+        }
+    };
 
     tableContainer.dataTable({
-        // Setup for Bootstrap support.
-        sDom: "<'row'<'col-12'f><'col-12'l>r>t<'row'<'col-12'i><'col-12'p>>",
-        sPaginationType: 'bootstrap',
         bProcessing: true,
         sAjaxSource: '/tracking.json',
         oLanguage      : {
@@ -34,23 +32,12 @@ $(document).ready(function() {
                 aTargets: [1, 2]
             }, {
                 mRender: dateRenderer,
-                aTargets: [3, 6, 7]
+                aTargets: [3, 6]
+            }, 
+            {
+                mRender: paymentRenderer,
+                aTargets: [7]
             }
         ],
-        // Setup for responsive datatables helper.
-        bAutoWidth     : false,
-        fnPreDrawCallback: function () {
-            // Initialize the responsive datatables helper once.
-            if (!responsiveHelper) {
-                responsiveHelper = new ResponsiveDatatablesHelper(tableContainer, breakpointDefinition);
-            }
-        },
-        fnRowCallback  : function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-            responsiveHelper.createExpandIcon(nRow);
-        },
-        fnDrawCallback : function (oSettings) {
-            responsiveHelper.respond();
-        }
     });
-
 });
