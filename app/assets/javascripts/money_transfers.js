e94d89a288116178537f8b7eed31f3c7f7f07449
Alexey Ershov <alex4rom@gmail.com>
2014-01-07 16:25:34 +0300

    Fix money_transfer renderers

--- app/assets/javascripts/money_transfers.js
+++ app/assets/javascripts/money_transfers.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 
-  var tableContainer = $('#money_transfers');
+    var tableContainer = $('#money_transfers');
 
-  tableContainer.dataTable({
-    sPaginationType: 'bootstrap',
-    bProcessing: true,
-    sAjaxSource: '/money_transfers.datatable',
-    oLanguage: {
-      sUrl: '/i18n/datatable.json'
-    },
+    tableContainer.dataTable({
+        sPaginationType: 'bootstrap',
+        bProcessing: true,
+        sAjaxSource: '/money_transfers.datatable',
+        oLanguage: {
+        sUrl: '/i18n/datatable.json'
+        },
 
-    aoColumns: [
-      {
-        mData: "sender",
-        mRender: renderers.user
-      }, {
-        mData: "receiver",
-        mRender: renderers.user
-      }, {
-        mData: "amount"
-      }, {
-        mData: "created_at",
-        mRender: renderers.date
-      },
-    ]
-  });
+        aoColumns: [
+            {
+                mData: "sender",
+                mRender: renderers.user
+            }, {
+                mData: "receiver",
+                mRender: renderers.user
+            }, {
+                mData: "amount"
+            }, {
+                mData: "created_at",
+                mRender: renderers.date
+            },
+        ]
+    });
 });
