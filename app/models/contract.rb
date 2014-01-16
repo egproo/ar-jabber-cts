cbbcd743f8ac225d44767d8e46f91c70d391caaf
Artem Sheremet <dot.doom@gmail.com>
2014-01-16 22:58:53 +0300

    Fix #2: valid name uniq on active contracts only

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -12,14 +12,19 @@ class Contract < ActiveRecord::Base
 
   accepts_nested_attributes_for :buyer, :seller, :payments
 
-  validates :name, uniqueness: true
+  validates :name,
+    uniqueness: {
+      scope: :active,
+      if: :active
+    },
+    presence: true,
+    format: { with: /.@conference.syriatalk.biz\z/ }
   validates :type, presence: true
   validates :buyer, presence: true
   validates :seller, presence: true
-  validates :name, presence: true
-  validates :name, format: { with: /.@conference.syriatalk.biz\z/ }
-  validates :duration_months, presence: true
-  validates :duration_months, inclusion: { in: (1..12), message: "from 1 to 12" }
+  validates :duration_months,
+    presence: true,
+    inclusion: { in: (1..12), message: "from 1 to 12" }
 
   def next_payment_date
     last_payment.try(:money_transfer).try(:received_at).try(:+, duration_months.months) if duration_months
