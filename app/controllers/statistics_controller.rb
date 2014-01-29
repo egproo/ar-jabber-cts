cca2b0c100f0c354d927c4fa7a5b4801c4b38a3d
Alexey Ershov <alex4rom@gmail.com>
2014-01-30 00:29:26 +0300

    Fix#18: Add income chart

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,7 +1,7 @@
 class StatisticsController < ApplicationController
   def income
     @income = MoneyTransfer.all(order: 'received_at ASC').each_with_object(Hash.new(0)) do |mt, chart|
-      chart[mt.received_at.to_date] += mt.amount
+      chart[mt.received_at.to_date.to_datetime.to_i * 1000] += mt.amount
     end
   end
 end
