5f0097c4dde3784f2342f66ed80bbfd0b38564b6
Artem Sheremet <dot.doom@gmail.com>
2014-02-02 23:26:55 +0300

    Add "create room" to Sync

--- app/controllers/ejabberd_controller.rb
+++ app/controllers/ejabberd_controller.rb
@@ -1,11 +1,14 @@
 class EjabberdController < ApplicationController
+  OWL = 5
+
   def sync
-    @transactions = Ejabberd.new.room_names('syriatalk.biz').split.map do |room_name|
+    server_rooms = Ejabberd.new.room_names('syriatalk.biz').split
+    @transactions = server_rooms.map do |room_name|
       tracked_rooms = Room.find_all_by_name(room_name)
       if tracked_rooms.present?
         if tracked_room = tracked_rooms.find(&:active)
           effective_to = tracked_room.last_payment.effective_to
-          if effective_to < 5.days.ago
+          if effective_to < OWL.days.ago
             ["#{room_name} destroy (paid until #{effective_to.to_date})", tracked_room]
           end
         else
@@ -14,6 +17,9 @@ class EjabberdController < ApplicationController
       else
         "#{room_name} destroy (not tracked)"
       end
-    end.compact
+    end.compact +
+      Room.active.all(conditions: ['name not in (?)', server_rooms], include: :last_payment).select do |r|
+        r.last_payment.effective_to >= OWL.days.ago
+      end.map { |r| ["#{r.name} create", r] }
   end
 end
