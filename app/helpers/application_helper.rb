2b27f9a690ac7c83eede7691d802be58e0fafb32
Artem Sheremet <dot.doom@gmail.com>
2014-02-06 12:32:03 +0300

    Make page title a bit more descriptive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  RESOURCE_ACTIONS_TITLE_MAP = {
+    'new' => 'New %s',
+    'create' => 'New %s',
+    'edit' => 'Edit %s',
+    'update' => 'Edit %s',
+    'show' => '%s info',
+  }
+
+  def resources_page_title
+    subject_name = params[:controller].titleize
+    if singleton_title_template = RESOURCE_ACTIONS_TITLE_MAP[params[:action]]
+      singleton_title_template % subject_name.singularize
+    else
+      subject_name
+    end
+  end
 end
