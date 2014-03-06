458742a7f1776144a53aab663c63a88a03293c1b
Alexey Ershov <alex4rom@gmail.com>
2014-03-06 22:31:35 +0300

    Simplify activities controller, map activities in view

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,23 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Audited::Adapters::ActiveRecord::Audit.includes(:user).all.map do |r|
-      {
-        action: r.action,
-        associated_id: r.associated_id,
-        associated_type: r.associated_type,
-        auditable_id: r.auditable_id,
-        auditable_type: r.auditable_type,
-        auditable: r.auditable,
-        user: r.user.try(:name) || 'Rails Console',
-        audited_changes: r.audited_changes.reject { |k| k == "adhoc_data" || k == "encrypted_password" },
-        comment: r.comment,
-        created_at: r.created_at,
-        id: r.id,
-        user_id: r.user_id,
-        user_type: r.user_type,
-        username: r.username,
-        version: r.version
-      }
-    end
+    @activities = Audited::Adapters::ActiveRecord::Audit.includes(:user).all
   end
 end
