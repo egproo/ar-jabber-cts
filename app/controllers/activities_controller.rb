6a25354aa4146a5668f37bd608a2820b9b15d963
Artem Sheremet <dot.doom@gmail.com>
2014-03-25 01:42:17 +0300

    Limit Activities size, rm public_activity traces

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Audited::Adapters::ActiveRecord::Audit.includes(:user, :auditable).all
+    @activities = Audited::Adapters::ActiveRecord::Audit.includes(:user, :auditable).order(created_at: :desc).limit(200)
   end
 end
--- app/helpers/public_activity_helper.rb
+++
@@ -1,2 +0,0 @@
-module PublicActivityHelper
-end
