2b9e4d2af98f16ee2b2a4eb2167fc7b64c9216d1
Artem Sheremet <dot.doom@gmail.com>
2014-04-02 16:43:49 +0300

    Announcements: allow no user to view all untracked

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,14 +2,12 @@ class Announcement < Contract
   attr_accessible :adhoc_data
   validates :adhoc_data, presence: true
 
-  scope :untracked, lambda { |user|
-    {
-      conditions: {
-        seller_id: user,
-        buyer_id: user,
-        active: false,
-      }
-    }
+  scope :untracked, proc { |user|
+    if user
+      where(seller_id: user, buyer_id: user, active: false)
+    else
+      where('seller_id = buyer_id AND active = ?', false)
+    end
   }
 
   def to_s
