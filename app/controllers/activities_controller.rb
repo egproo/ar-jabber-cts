a56d37843368e151c21e59e6088331920857b1fa
Artem Sheremet <dot.doom@gmail.com>
2014-04-07 22:35:53 +0300

    CanCan for Activities and Navigation

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,7 @@
 class ActivitiesController < ApplicationController
+  load_and_authorize_resource :activity, class: 'Audited::Adapters::ActiveRecord::Audit'
+
   def index
-    @activities = Audited::Adapters::ActiveRecord::Audit.includes(:user, :auditable).order('created_at DESC').limit(200)
+    @activities = @activities.includes(:user, :auditable).order('created_at DESC').limit(200)
   end
 end
