a10fe53e36756cab7d95a0b892d0c9335aa487c0
Artem Sheremet <dot.doom@gmail.com>
2014-04-07 23:34:53 +0300

    Do not hide payment form for new contracts

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,31 +1,31 @@
 $(document).on('decorate', function(e, updated) {
     $('.room-name').input_field_addons({ after: '@...' });
 
-    var seller_typeahead = $('.typeahead-user[name="room[buyer_attributes][name]"]');
-    var seller_info_div = function(seller_input) {
-        return seller_input.parent().parent().parent().next();
+    var buyer_typeahead = $('.typeahead-user[name="room[buyer_attributes][name]"]');
+    var buyer_info_div = function(buyer_input) {
+        return buyer_input.parent().parent().parent().next();
     };
 
     var payment_fs = $('#payment_fs');
 
-    seller_typeahead.on('typeahead:uservalue', function(e, value) {
-        if (value) {
-            seller_info_div($(e.target)).slideDown();
-        }
-        if (value === original_buyer) {
+    var buyer_changed = function(value) {
+        if (original_buyer && value === original_buyer) {
             payment_fs.slideUp();
         } else {
             payment_fs.slideDown();
         }
+    };
+
+    buyer_typeahead.on('typeahead:uservalue', function(e, value) {
+        if (value) {
+            buyer_info_div($(e.target)).slideDown();
+        }
+        buyer_changed(value);
     });
 
-    seller_typeahead.on('typeahead:selected', function(e, value) {
-        seller_info_div($(e.target)).slideUp();
-        if (value === original_buyer) {
-            payment_fs.slideUp();
-        } else {
-            payment_fs.slideDown();
-        }
+    buyer_typeahead.on('typeahead:selected', function(e, value) {
+        buyer_info_div($(e.target)).slideUp();
+        buyer_changed(value);
     });
 });
 
