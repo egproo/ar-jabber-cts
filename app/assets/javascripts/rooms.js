dcbf198ae72a7bb7dda2172f6ab1ae26b351582e
Artem Sheremet <dot.doom@gmail.com>
2014-04-12 18:35:50 +0300

    Contract user typeahead: add signals on decorate

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,9 +1,10 @@
 $(document).on('decorate', function(e, updated) {
     $('.room-name').input_field_addons({ after: '@...' });
-});
 
-$(document).ready(function() {
-    $document = $(document);
+    var buyer_typeahead = $('.typeahead-user[name="room[buyer_attributes][jid]"]');
+    var buyer_info_div = function(buyer_input) {
+        return buyer_input.parent().parent().parent().next();
+    };
 
     var payment_fs = $('#payment_fs');
 
@@ -15,29 +16,26 @@ $(document).ready(function() {
         }
     };
 
-    var buyer_typeahead_selector = '.typeahead-user[name="room[buyer_attributes][jid]"]';
-    var buyer_info_div = function(buyer_input) {
-        return buyer_input.parent().parent().parent().next();
-    };
-
-    $document.on('typeahead:uservalue', buyer_typeahead_selector, function(e, value) {
+    buyer_typeahead.on('typeahead:uservalue', function(e, value) {
         if (value) {
             buyer_info_div($(e.target)).slideDown();
         }
         buyer_changed(value);
     });
 
-    $document.on('typeahead:selected', buyer_typeahead_selector, function(e, value) {
+    buyer_typeahead.on('typeahead:selected', function(e, value) {
         buyer_info_div($(e.target)).slideUp();
         buyer_changed(value);
     });
+});
 
+$(document).ready(function() {
     $('form').on('ajax:success', function(e, data, status, xhr) {
         window.location.href = data.location;
     });
 
     $('form').on('ajax:error', function(e, xhr, status, error) {
         $(this).html(xhr.responseText);
-        $document.trigger('decorate', this);
+        $(document).trigger('decorate', this);
     });
 });
