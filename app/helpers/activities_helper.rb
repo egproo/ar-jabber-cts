a9c061e1e5a100d284d18e5f7e6be542a921a455
Alexey Ershov <alex4rom@gmail.com>
2014-04-14 01:16:46 +0300

    Fix#1: Activities displays 'not found'

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -13,9 +13,14 @@ module ActivitiesHelper
   STRIPPED_FIELDS = %w(adhoc_data encrypted_password)
 
   def decorate(model, key, value)
+    # f.e. MODEL_ASSOCIATION_CLASSES['Contract'][:buyer_id]
     if value && model = MODEL_ASSOCIATION_CLASSES[model].try(:[], key.to_sym)
-      value = model.find(value)
-      link_to value, value
+      # If user with this ID (value) exists
+      if value = model.find_by_id(value)
+        link_to value, value
+      else
+        'not found'
+      end
     else
       value.nil? ? "nil" : value
     end
@@ -28,7 +33,7 @@ module ActivitiesHelper
       else
         "#{key}: #{decorate(model, key, value[0])} => #{decorate(model, key, value[1])}"
       end
-    elsif (value && value != "") && (STRIPPED_FIELDS.include?(key))
+    elsif value.present? && (STRIPPED_FIELDS.include?(key))
       "#{key}: has been set"
     elsif STRIPPED_FIELDS.include?(key)
       "#{key}: has been cleared"
