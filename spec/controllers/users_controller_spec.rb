9216ef7cf0e0b702187c0806ecc20d709c7f6f8d
Alexey Ershov <alex4rom@gmail.com>
2014-04-24 21:23:24 +0300

    Add users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe UsersController do
 
   let(:user) { FactoryGirl.create(:user) }
-  before { sign_in user }
+  let(:admin) { FactoryGirl.create(:admin) }
+
+  before(:each) do
+    sign_in admin
+  end
 
   describe 'GET #index' do
     it 'has a 200 status code' do
@@ -11,11 +15,11 @@ describe UsersController do
       expect(response.status).to eq(200)
     end
 
-    # TODO: make test pass
-    #it 'assigns all users to @users' do
-      #get :index
-      #expect(assigns(:users)).to match_array([user])
-    #end
+    it 'assigns all users to @users' do
+      # @users contain two created users
+      get :index
+      expect(assigns(:users)).to match_array([admin, user])
+    end
   end
 
   describe 'GET #show' do
@@ -27,6 +31,28 @@ describe UsersController do
     it 'assigns the requested user to @user' do
       get :show, id: user
       expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe '#switch' do
+    it 'changes @effective_user' do
+      get :show, id: user
+      expect(session[:effective_user_id]).to eq(nil)
+
+      get :switch, id: user
+      expect(session[:effective_user_id]).to eq(user.id.to_s)
+    end
+
+    it 'redirects to the root' do
+      get :switch, id: user
+      expect(response).to redirect_to :root
+    end
+
+    describe '#update' do
+      it 'redirects to the @user' do
+        put :update, id: user, user: FactoryGirl.attributes_for(:user)
+        expect(response).to redirect_to user
+      end
     end
   end
 
